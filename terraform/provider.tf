3cbf4e36ebf89230a56a7f8a9843cec76aae3e34
Hero-Xero <omarahmedasaad@gmail.com>
2026-06-28 05:49:53 +0300

    Installed promtheous and grafana, changed db_password, added service monitor to scrap the app's metrics

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,6 +6,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
   }
 }
 
@@ -13,4 +18,17 @@ provider "aws" {
   region = "us-east-1"
   # Credentials are read from environment variables:
   # AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
+}
+
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    }
+  }
 }
